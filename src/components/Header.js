92ec3a78ba190da29fdeee3d41b014e57b9d3af1
unknown <jacqui.longhurst@btinternet.com>
2022-01-27 16:24:53 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,43 @@
-import { AppBar, Container, Select, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Header = () => {
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Monserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+
+    },
+}))
+
+
+const Header = () => { 
+    const classes = useStyles();
+    // useHistory changed in v6 of react router to useNavigate
+    const navigate = useNavigate();
   return (
       <AppBar color='transparent' position='static'>
           <Container>
               <Toolbar>
-                  <Typography>
+                  <Typography 
+                  onclick={() => navigate("/")} 
+                  className={classes.title}
+                  >
                       CryptoZone
                   </Typography>
-                  <Select>
-
+                  <Select variant='outlined'
+                  style ={{
+                      width: 100, 
+                      height: 40,
+                      marginLeft: 15,
+                  }}
+                  >
+                    <MenuItem>USD</MenuItem>
+                    <MenuItem>EUR</MenuItem>
                   </Select>
               </Toolbar>
           </Container>
